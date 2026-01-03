cc461adc6fda05e2afe50d80964c62fe75f3e71a
hzla <neohzla@gmail.com>
2026-01-02 22:25:38 -0500

    update sets

--- calc/data/species.js
+++ calc/data/species.js
@@ -9240,6 +9240,12 @@ var RR_PATCH = {
         weightkg: 32.5,
         abilities: { 0: 'Refriegerate' }
     },
+    'Quivern': {
+        types: ['Ice', 'Dragon'],
+        bs: { hp: 85, at: 100, df: 74, sa: 45, sd: 65, sp: 111 },
+        weightkg: 32.5,
+        abilities: { 0: 'Refriegerate' }
+    },
     'Clauncher-Sevii': {
         types: ['Bug', 'Dark'],
         bs: { hp: 50, at: 38, df: 62, sa: 58, sd: 63, sp: 59 },
