ec093224192c8eda456f35b1973f4b946b7eabf6
hzla <neohzla@gmail.com>
2026-01-13 14:59:26 -0500

    update sets

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -543,29 +543,37 @@ function showMoveExtras(moveObj, ppObj=null, fullSetName="", index=null) {
 		moveGroupObj.children(".stat-drops").hide();
 	}
 	moveGroupObj.children(".move-z").prop("checked", false);
+
+
+
+	if (TITLE.includes("Cascade")) {
+		setTimeout(function() {
+			if ($(moveObj).parent().hasClass('move1')) {
+				let pokeInfo = $(moveGroupObj).parents('.poke-info')
+				let itemName = pokeInfo.find('.item').val()
+				if (itemName) {
+					if (itemName.includes("Tera ")) {
+						teraType = move.type
+						pokeInfo.find(".type1").val(teraType).css('border', '1px solid #bb86fc')
+						pokeInfo.find('.type2').val("")
+					} else {
+						let pokeName = pokeInfo.find('.select2-chosen').text().split(" (")[0]
+						if (pokedex[pokeName]) {
+							let oldTypes = pokedex[pokeName].types
+							pokeInfo.find('.type1').val(oldTypes[0]).css('border', '')
+							if (oldTypes.length > 1) {
+								pokeInfo.find('.type2').val(oldTypes[1])
+							}
+						}
+						
+					}
+				}
+			}
+		}, 500)
+		
+
+	}
 	
-	
-	// if ($(moveObj).parent().hasClass('move1')) {
-	// 	let pokeInfo = $(moveGroupObj).parents('.poke-info')
-	// 	let itemName = pokeInfo.find('.item').val()
-	// 	if (itemName) {
-	// 		if (itemName.includes("Tera ")) {
-	// 			teraType = move.type
-	// 			pokeInfo.find(".type1").val(teraType).css('border', '1px solid #bb86fc')
-	// 			pokeInfo.find('.type2').val("")
-	// 		} else {
-	// 			let pokeName = pokeInfo.find('.select2-chosen').text().split(" (")[0]
-	// 			if (pokedex[pokeName]) {
-	// 				let oldTypes = pokedex[pokeName].types
-	// 				pokeInfo.find('.type1').val(oldTypes[0]).css('border', '')
-	// 				if (oldTypes.length > 1) {
-	// 					pokeInfo.find('.type2').val(oldTypes[1])
-	// 				}
-	// 			}
-				
-	// 		}
-	// 	}
-	// }
 
 	
 
