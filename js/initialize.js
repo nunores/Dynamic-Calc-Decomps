ad5807993dd6a5b1a13fc063cc890b4c9504d118
hzla <neohzla@gmail.com>
2026-03-03 17:36:56 -0500

    add better basegame detectionf or plat

--- js/initialize.js
+++ js/initialize.js
@@ -398,7 +398,7 @@ function setBaseGame(title) {
           $('.trainer-pok-list.opposing').addClass('ai-show')
         }
         $('#sync-lua').show()
-    } else if (TITLE == "Renegade Platinum") {
+    } else if (TITLE.includes("Platinum") ) {
       baseGame = "Pt"
     } else if (TITLE.includes(" Black") || TITLE.includes(" White")) {
       baseGame = "BW"
